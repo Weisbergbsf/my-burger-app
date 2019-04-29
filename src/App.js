d7c0586af8e418042cf8cfe0e6a7a3c6b2dbc727
Weisberg Barros <weisbergbsf@gmail.com>
2019-04-28 21:20:28 -0300

    Created Burger component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import Layout from './components/Layout/Layout'
+import Layout from './components/Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
 function App() {
   return (
     <div >
       <Layout>
-        <h1>here...</h1>
+        <BurgerBuilder />
       </Layout>
     </div>
   );
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Aux from '../../hoc/Aux';
+import './Layout.css'
 
 const layout = (props) => (
     <Aux>
         <div>Toolbar, SideDrawer, Backdrop</div>
-        <main>
+        <main className="Content">
             {props.children}
         </main>
     </Aux>
