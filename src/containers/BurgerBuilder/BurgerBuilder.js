00aad2761656c708891af0578205a72c0ca2cc55
Weisberg Barros <weisbergbsf@gmail.com>
2019-05-18 19:37:23 -0300

    Implemented the Backdrop Component

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -61,6 +61,10 @@ class BurgerBuilder extends Component {
         this.setState({ purchasing: true })
     }
 
+    purchaseCancelHandler = () => {
+        this.setState({ purchasing: false })
+    }
+
     updatePurchaseState(ingedients) {
         const sum = Object.keys(ingedients)
             .map(igKey => {
@@ -81,7 +85,7 @@ class BurgerBuilder extends Component {
         }
         return (
             <Aux>
-                <Modal show={this.state.purchasing}>
+                <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
                     <OrderSummary ingredients={this.state.ingredients}/>
                 </Modal>
                 <Burger ingredients={this.state.ingredients}/>
