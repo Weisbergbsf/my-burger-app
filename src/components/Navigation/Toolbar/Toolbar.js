46d7240e2c26d791ecb2e813d3e8982a38cdf782
Weisberg Barros <weisberg.fontes@nlt.com.br>
2019-05-22 21:29:56 -0300

    Added Sidedrawer Toggle Button

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -2,12 +2,13 @@ import React from "react";
 
 import NavigationItems from "../NavigationItems/NavigationItems";
 import Logo from "../../Logo/Logo";
+import DrawerToggle from "../SideDrawer/DrawerToggle/DrawerToggle";
 
 import "./Toolbar.css";
 
 const toolbar = props => (
   <header className="Toolbar">
-    <div>MENU</div>
+    <DrawerToggle clicked={props.drawerToggleClicked} />
     <div className="Logo">
       <Logo />
     </div>
