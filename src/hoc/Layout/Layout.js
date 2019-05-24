51a9b3f92aba7c0b31bef7aca02a008e00c00d25
Weisberg Barros <weisberg.fontes@nlt.com.br>
2019-05-23 21:12:02 -0300

    Changed the Folder Structure

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
-import Aux from "../../hoc/Aux";
-import Toolbar from "../Navigation/Toolbar/Toolbar";
-import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
+import Aux from "../Aux/Aux";
+import Toolbar from "../../components/Navigation/Toolbar/Toolbar";
+import SideDrawer from "../../components/Navigation/SideDrawer/SideDrawer";
 import "./Layout.css";
 
 class Layout extends Component {
