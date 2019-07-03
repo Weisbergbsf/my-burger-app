23fc6fc61af17d6662bd2f80ff9b26c7e6494d73
Weisberg Barros <weisberg.fontes@nlt.com.br>
2019-07-02 21:42:56 -0300

    Added custom form validation

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React from "react";
 
-import './Button.css'
+import "./Button.css";
 
-const button = (props) => (
-    <button className={[`Button ${props.btnType}`].join(' ')} onClick={props.clicked}>{props.children}</button>
+const button = props => (
+  <button
+    disabled={props.disabled}
+    className={[`Button ${props.btnType}`].join(" ")}
+    onClick={props.clicked}
+  >
+    {props.children}
+  </button>
 );
 
 export default button;
--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -4,16 +4,57 @@ import "./Input.css";
 
 const input = props => {
   let inputElement = null;
+  const inputClasses = ["InputElement"];
 
-  switch (props.inputtype) {
+  if (props.invalid && props.shouldValidate && props.touched) {
+    inputClasses.push("Invalid");
+  }
+
+  switch (props.elementType) {
     case "input":
-      inputElement = <input className="InputElement" {...props} />;
+      inputElement = (
+        <input
+          className={inputClasses.join(" ")}
+          {...props.elementConfig}
+          value={props.value}
+          onChange={props.changed}
+        />
+      );
       break;
     case "textarea":
-      inputElement = <textarea className="InputElement" {...props} />;
+      inputElement = (
+        <textarea
+          className={inputClasses.join(" ")}
+          {...props.elementConfig}
+          value={props.value}
+          onChange={props.changed}
+        />
+      );
+      break;
+    case "select":
+      inputElement = (
+        <select
+          className={inputClasses.join(" ")}
+          value={props.value}
+          onChange={props.changed}
+        >
+          {props.elementConfig.options.map(option => (
+            <option key={option.value} value={option.value}>
+              {option.displayValue}
+            </option>
+          ))}
+        </select>
+      );
       break;
     default:
-      inputElement = <input className="InputElement" {...props} />;
+      inputElement = (
+        <input
+          className={inputClasses.join(" ")}
+          {...props.elementConfig}
+          value={props.value}
+          onChange={props.changed}
+        />
+      );
   }
 
   return (
